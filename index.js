09f2e4f09b9df85c60b685f90ea638a24b21507b
calvincgee <50720125+calvincgee@users.noreply.github.com>
2020-08-24 17:37:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var activities = ['developing new projects', 'programming', 'building a website', 'analyzing data', 'rock climbing', 'jumping off a cliff']
 
-document.getElementById('activityBox').innerHTML = activities[0];
-
 var changeActivity = setInterval(changeNum, 3000);
 
 function changeNum() {
