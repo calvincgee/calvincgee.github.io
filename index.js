dd84e1c4f34659898adfdcf5b086c44e10d845d1
calvincgee <50720125+calvincgee@users.noreply.github.com>
2020-08-24 20:43:48 -0700

    updates

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var activities = ['developing new projects', 'programming', 'building a website', 'analyzing data', 'rock climbing', 'jumping off a cliff']
+var activities = ['developing new projects', 'programming', 'building a website', 'analyzing data', 'fixing bugs', 'watching basketball', 'rock climbing', 'jumping off a cliff']
 
 var changeActivity = setInterval(changeNum, 2000);
 var index = 0;
 function changeNum() {
     document.getElementById('activityBox').innerHTML = activities[index % activities.length]
+    index++;
 }
